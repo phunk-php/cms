7f48841913ec0012b3cc025ea992d2c5655b25ea
Jack Jackdaw <j@ckdaw.net>
2026-09-01 08:27:05 +0300

    Added banner mehod.

--- src/MediaExtension.php
+++ src/MediaExtension.php
@@ -2,16 +2,27 @@
 
 namespace Phunk\Cms;
 
+use Phunk\Cms\Entity\BaseContent;
+use Phunk\Cms\MediaService;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class MediaExtension extends AbstractExtension
 {
+    public function __construct(private MediaService $media)
+    {}
+
     public function getFunctions(): array
     {
         return [
             new TwigFunction('imageUrl', $this->imageUrl(...)),
+            new TwigFunction('banner', $this->banner(...)),
         ];
+    }
+
+    public function banner(BaseContent $content): ?BaseContent
+    {
+        return $this->media->findBanner($content)->unwrapOr(null);
     }
 
     public function imageUrl(string $slug, array $options = []): string
