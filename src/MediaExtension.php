471cfececade8251d1a926655957c316b0e0e077
Jack Jackdaw <j@ckdaw.net>
2026-09-02 19:45:52 +0300

    Added imagePath and imageUrl methods and use named routes to generate image path.

--- src/MediaExtension.php
+++ src/MediaExtension.php
@@ -4,18 +4,24 @@ namespace Phunk\Cms;
 
 use Phunk\Cms\Entity\BaseContent;
 use Phunk\Cms\MediaService;
+use Soap\Url;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class MediaExtension extends AbstractExtension
 {
-    public function __construct(private MediaService $media)
-    {}
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private MediaService $media,
+    ) {
+    }
 
     public function getFunctions(): array
     {
         return [
             new TwigFunction('imageUrl', $this->imageUrl(...)),
+            new TwigFunction('imagePath', $this->imagePath(...)),
             new TwigFunction('banner', $this->banner(...)),
         ];
     }
@@ -25,11 +31,25 @@ class MediaExtension extends AbstractExtension
         return $this->media->findBanner($content)->unwrapOr(null);
     }
 
+    public function imagePath(string $slug, array $options = []): string
+    {
+        return $this->getImageUrl($slug, $options, UrlGeneratorInterface::ABSOLUTE_PATH);
+    }
+
     public function imageUrl(string $slug, array $options = []): string
+    {
+        return $this->getImageUrl($slug, $options, UrlGeneratorInterface::ABSOLUTE_URL);
+    }
+
+    private function getImageUrl(string $slug, array $options, int $mode): string
     {
         $defaults = ['size' => 'default'];
         $options = array_merge($defaults, $options);
 
-        return '/image/' . $options['size'] . '/' . $slug;
+        return $this->urlGenerator->generate(
+            'image',
+            ['size' => $options['size'], 'slug' => $slug],
+            $mode,
+        );
     }
 }
